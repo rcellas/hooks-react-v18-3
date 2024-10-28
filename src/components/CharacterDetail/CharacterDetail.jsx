0c0109b9ead3cfd81e41e7ba9c72baa40c0fc73b
rcellas <rocio.cejudo.soronellas@gmail.com>
2024-10-28 13:05:27 +0100

    add id in character detail

--- src/components/CharacterDetail/CharacterDetail.jsx
+++ src/components/CharacterDetail/CharacterDetail.jsx
@@ -1,8 +1,7 @@
 
-
 function CharacterDetail({character}) {
   return (
-    <div>
+    <div key={character.id}>
         <img src={character.image} alt={character.name} />
         <h1>{character.name}</h1>
     </div>
--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -5,10 +5,14 @@ import CharacterDetail from "../CharacterDetail/CharacterDetail"
 function Characters() {
     const [characters, setCharacters]=useState([])
     useEffect(()=>{
-        fetch("https://rickandmortyapi.com/api/character")
-            .then((response)=>response.json())
-        .then((data)=>setCharacters(data.results))
+       getData()
     },[])
+    const getData = () => {
+      fetch("https://rickandmortyapi.com/api/character")
+      .then(response => response.json())
+      .then((data)=>setCharacters(data.results))
+      .catch(error => console.log("Mensaje de error: ", error))
+  } 
   return (
     <div>
         {characters.map((character)=>{
